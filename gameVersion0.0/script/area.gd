5240768513d06ac76fac578fbc576b8d95d0421e
AlexisUsuga <bralcaus@gmail.com>
2023-03-21 16:42:52 -0500

    agreagando el proyecto

---
+++ gameVersion0.0/script/area.gd
@@ -0,0 +1,11 @@
+extends Area2D
+#export(PackedScene) var ecena esto sirve para obtener la ecena seleccionada
+
+func _ready():
+	pass
+	#print(ecena)
+	
+	#var e = ecena.instance() aqui la creamos o la instaciamos
+	#add_child(e) y la agregamos como hijo para que se pueda ejecutar
+
+
